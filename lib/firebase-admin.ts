be923ab1448a9195415ea7cc44d594c11329c9f9
iamtushar28 <tusharsuryawanshi2232@gmail.com>
2026-06-19 12:44:21 +0530

    add: env change

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -7,7 +7,7 @@ const app =
     ? getApps()[0]
     : initializeApp({
         credential: cert({
-          projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+          projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
           privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
             /\\n/g,
